83f9b743185d4c9a2cb80223d6d8fd23693b5caf
Safayet Mahid <safayetmahid96@gmail.com>
2022-03-12 14:33:11 +0600

    further pages added

--- components/Property.js
+++ components/Property.js
@@ -1,42 +1,40 @@
-import React from 'react';
-import Link from "next/link"
-import { Avatar, Box, Flex, Text } from "@chakra-ui/react"
-import Image from "next/image"
-import defaultImage from "../assets/house.jpg"
+import Link from 'next/link';
+import Image from 'next/image';
+import { Box, Flex, Text } from '@chakra-ui/layout';
+import { Avatar } from '@chakra-ui/avatar';
+import { FaBed, FaBath } from 'react-icons/fa';
+import { BsGridFill } from 'react-icons/bs';
 import { GoVerified } from 'react-icons/go';
-import { FaBath, FaBed } from "react-icons/fa"
-import { BsGridFill } from "react-icons/bs"
 import millify from 'millify';
 
-const Property = ({ property: { title, coverPhoto, externalID, isVerified, price, rentFrequency, rooms, baths, agency, area } }) => {
-    return (
-        <Link href={`/property/${externalID}`} passHref>
-            <Flex flexWrap='wrap' w='420px' p='5' paddingTop='0px' justifyContent='flex-start' cursor='pointer'>
-                <Box>
-                    <Image src={coverPhoto ? coverPhoto.url : defaultImage} width={400} height={260} alt="house" />
-                </Box>
-                <Box w="full">
-                    <Flex alignItems="center" paddingTop="2" justifyContent="space-between">
-                        <Flex alignItems="center">
-                            <Box color="green.400" paddingRight='3' > {isVerified && <GoVerified />} </Box>
-                            <Text fontWeight="bold" fontSize="lg"> AED {millify(price)} {rentFrequency && `/${rentFrequency}`} </Text>
-                        </Flex>
-                        <Box>
-                            <Avatar size="sm" src={agency?.logo?.url}></Avatar>
-                        </Box>
-                    </Flex>
-                    <Flex alignItems="center" p='1' justifyContent="space-between" color="blue.400" w="250px">
-                        {rooms} <FaBed /> | {baths} <FaBath /> | {millify(area)} sqft <BsGridFill />
-                    </Flex>
-                    <Text fontSize="lg" >
-                        {title.length > 30 ? title.substring(0, 30) + "..." : title}
-                    </Text>
+import DefaultImage from '../assets/house.jpg';
 
-                </Box>
-            </Flex >
-
-        </Link >
-    );
-};
+const Property = ({ property: { coverPhoto, price, rentFrequency, rooms, title, baths, area, agency, isVerified, externalID } }) => (
+    <Link href={`/property/${externalID}`} passHref>
+        <Flex flexWrap='wrap' w='420px' p='5' paddingTop='0px' justifyContent='flex-start' cursor='pointer' >
+            <Box>
+                <Image alt="property" src={coverPhoto ? coverPhoto.url : DefaultImage} width={400} height={260} />
+            </Box>
+            <Box w='full'>
+                <Flex paddingTop='2' alignItems='center' justifyContent='space-between'>
+                    <Flex alignItems='center'>
+                        <Box paddingRight='3' color='green.400'>{isVerified && <GoVerified />}</Box>
+                        <Text fontWeight='bold' fontSize='lg'>AED {price}{rentFrequency && `/${rentFrequency}`}</Text>
+                    </Flex>
+                    <Box>
+                        <Avatar size='sm' src={agency?.logo?.url}></Avatar>
+                    </Box>
+                </Flex>
+                <Flex alignItems='center' p='1' justifyContent='space-between' w='250px' color='blue.400'>
+                    {rooms}
+                    <FaBed /> | {baths} <FaBath /> | {millify(area)} sqft <BsGridFill />
+                </Flex>
+                <Text fontSize='lg'>
+                    {title.length > 30 ? title.substring(0, 30) + '...' : title}
+                </Text>
+            </Box>
+        </Flex>
+    </Link>
+);
 
 export default Property;
